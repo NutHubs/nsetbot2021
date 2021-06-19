2e00c0f027ed9f726830da9df4c4ffafb7f7e420
NutHubs <31301535+NutHubs@users.noreply.github.com>
2021-06-20 02:55:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,43 @@ $_msg = $arrJson['events'][0]['message']['text'];
 
     	$arrPostData = array();
     	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	    $arrPostData['messages'][0]['type'] = "text";
-   	  $arrPostData['messages'][0]['text'] = 'อร่อยที่สุดในโลกเลยหล่ะ';
+	$arrPostData['messages'][0]['type'] = "template";
+	$arrPostData['messages'][0]['altText'] = "Special Command";
+   	$arrPostData['messages'][0]['template'] = [
+   		"type" => "template", 
+   		"altText" => "This is a buttons template", 
+   		"template" => [
+        	"type" => "buttons", 
+         	"thumbnailImageUrl" => "https://example.com/bot/images/image.jpg", 
+         	"imageAspectRatio" => "rectangle", 
+         	"imageSize" => "cover", 
+         	"imageBackgroundColor" => "#FFFFFF", 
+         	"title" => "Menu", 
+         	"text" => "Please select", 
+         	"defaultAction" => [
+            	"type" => "uri", 
+            	"label" => "View detail", 
+            	"uri" => "http://example.com/page/123" 
+         ], 
+         "actions" => [
+               [
+                  "type" => "postback", 
+                  "label" => "Buy", 
+                  "data" => "action=buy&itemid=123" 
+               ], 
+               [
+                     "type" => "postback", 
+                     "label" => "Add to cart", 
+                     "data" => "action=add&itemid=123" 
+               ], 
+               [
+                     "type" => "uri", 
+                     "label" => "View detail", 
+                     "uri" => "http://example.com/page/123" 
+               ] 
+            ] 
+      ] 
+]; 
 	  
     	$arrPostData['messages'][1]['type'] = "sticker";
     	$arrPostData['messages'][1]['packageId'] = "2";
